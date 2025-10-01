cab5eaacd9b044882d81238645a6408303186fbc
Vaibhav <vaibhavjam86@gmail.com>
2025-10-01 17:16:11 +0530

    update outputs file according to ec2 instance created via for_each

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,9 +1,30 @@
+# output "ec2_public_ip" {
+#   value = aws_instance.my_ec2[*].public_ip
+# }
+# output "ec2_public_dns" {
+#   value = aws_instance.my_ec2[*].public_dns
+# }
+# output "ec2_private_ip" {
+#   value = aws_instance.my_ec2[*].private_ip
+# }
+
+
+
 output "ec2_public_ip" {
-  value = aws_instance.my_ec2[*].public_ip
+    value =[
+     for instance in aws_instance.my_ec2 : instance.public_ip
+
+    ]
 }
 output "ec2_public_dns" {
-  value = aws_instance.my_ec2[*].public_dns
+    value =[
+     for instance in aws_instance.my_ec2 : instance.public_dns
+
+    ]
 }
 output "ec2_private_ip" {
-  value = aws_instance.my_ec2[*].private_ip
+    value =[
+     for instance in aws_instance.my_ec2 : instance.private_ip
+
+    ]
 }
