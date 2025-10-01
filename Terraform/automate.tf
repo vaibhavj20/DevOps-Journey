a6450165e5ed12a102e22c0112902d77bf4c6c64
Vaibhav <vaibhavjam86@gmail.com>
2025-10-01 15:58:32 +0530

    formatted

--- Terraform/automate.tf
+++ Terraform/automate.tf
@@ -1,4 +1,4 @@
-resource local_file my_file{
-    filename = "test.txt"
-    content = "This is a file created by Terraform"
+resource "local_file" "my_file" {
+  filename = "test.txt"
+  content  = "This is a file created by Terraform"
 }
