87b439b16921e4a05f9510f37b98d8089ae6ff01
Vaibhav <vaibhavjam86@gmail.com>
2025-10-01 17:19:21 +0530

    comment added

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,3 +1,5 @@
+# OUTPUTS FOR COUNT
+
 # output "ec2_public_ip" {
 #   value = aws_instance.my_ec2[*].public_ip
 # }
@@ -9,7 +11,7 @@
 # }
 
 
-
+# OUTPUTS FOR FOR_EACH
 output "ec2_public_ip" {
     value =[
      for instance in aws_instance.my_ec2 : instance.public_ip
