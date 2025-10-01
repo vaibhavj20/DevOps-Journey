ae30667aeb7e55f1630b77aaad13dbd9b9dc3bb3
Vaibhav <vaibhavjam86@gmail.com>
2025-10-01 12:00:45 +0530

    outputs file added includes pub,prv,public-den

---
+++ Terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "ec2_public_ip" {
+    value = aws_instance.my_ec2.public_ip
+}
+output "ec2_public_dns" {
+    value = aws_instance.my_ec2.public_dns
+}
+output "ec2_private_ip" {
+    value = aws_instance.my_ec2.private_ip
+}
