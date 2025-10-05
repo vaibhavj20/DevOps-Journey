34d310ae437527405d6c2581dee78a629ab8daed
Vaibhav <vaibhavjam86@gmail.com>
2025-10-05 20:01:32 +0530

    added files in prod

--- Terraform/terraform.tf
+++ Terraform/terraform.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket        = "my-infra-bucket-v062001"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "my-infra-table-v062001"
-}
+#   backend "s3" {
+#     bucket        = "my-infra-bucket-v062001"
+#     key            = "terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "my-infra-table-v062001"
+# }
 }
