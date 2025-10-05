a6bba796939ea7b78c8a414095495f20a39212d4
Vaibhav <vaibhavjam86@gmail.com>
2025-10-05 15:25:52 +0530

    Remote Backend

--- Terraform/terraform.tf
+++ Terraform/terraform.tf
@@ -5,4 +5,11 @@ terraform {
       version = "6.14.1"
     }
   }
+
+  backend "s3" {
+    bucket        = "my-infra-bucket-v062001"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "my-infra-table-v062001"
+}
 }
