6da24749df99b09763b49586a19445cb6552e6c6
Vaibhav <vaibhavjam86@gmail.com>
2025-10-10 09:22:50 +0530

    local variables

---
+++ Terraform-EKS-Cluster/locals.tf
@@ -0,0 +1,10 @@
+locals {
+    region = "us-east-1"
+    name = "my-eks-cluster"
+    vpc_cidr = "10.0.0.0/16"
+    azs = ["us-east-1a", "us-east-1b"]
+    public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+    private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+    intra_subnets = ["10.0.5.0/24", "10.0.6.0/24"]
+    env = "dev"
+}
